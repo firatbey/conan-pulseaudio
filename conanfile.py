663a1a10c63626bdc1de06eda34c56ea06f4f33e
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-01-17 21:49:06 +0100

    pulseaudio 13.0

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,74 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools, AutoToolsBuildEnvironment 
+from conans.errors import ConanInvalidConfiguration
 import os
+import glob
+import shutil
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
-    # Remove following lines if the target lib does not use CMake
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    name = "pulseaudio"
+    description = "PulseAudio is a sound system for POSIX OSes, meaning that it is a proxy for sound applications."
+    topics = ("conan", "pulseaudio", "sound")
+    url = "https://github.com/bincrafters/conan-pulseaudio"
+    homepage = "http://pulseaudio.org/"
+    license = "LGPL-2.1"
 
-    # Options may need to change depending on the packaged library
+    generators = 'pkg_config'
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_alsa": [True, False],
+        "with_glib": [True, False],
+        "with_fftw": [True, False],
+        "with_x11": [True, False],
+        "with_samplerate": [True, False],
+        "with_openssl": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_alsa": True,
+        "with_glib": True,
+        "with_fftw": True,
+        "with_x11": True,
+        "with_samplerate": True,
+        "with_openssl": True,
+    }
 
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
 
-    requires = (
-        "zlib/1.2.11"
-    )
+    _autotools = None
+
+    def configure(self):
+        if self.settings.compiler == "Visual Studio":
+            raise ConanInvalidConfiguration("The project libsndfile can not be built by Visual Studio.")
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
+
+    def requirements(self):
+        self.requires("libsndfile/1.0.28@bincrafters/stable")
+        if self.options.with_alsa:
+            self.requires("libalsa/1.1.9")
+        if self.options.with_glib:
+            self.requires("glib/2.58.3@bincrafters/stable")
+        if self.options.with_fftw:
+            self.requires("fftw/3.3.8")
+        if self.options.with_x11:
+            self.requires("libx11/1.6.8@bincrafters/stable")
+            self.requires("libxcb/1.13.1@bincrafters/stable")
+            self.requires("libice/1.0.10@bincrafters/stable")
+            self.requires("libsm/1.2.3@bincrafters/stable")
+            self.requires("libxtst/1.2.3@bincrafters/stable")
+        if self.options.with_samplerate:
+            self.requires("libsamplerate/0.1.9@bincrafters/stable")
+        if self.options.with_openssl:
+            self.requires("openssl/1.1.1d")   
+
+
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -34,29 +79,53 @@ class LibnameConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False  # example
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+    def _configure_autotools(self):
+        if not self._autotools:
+            self._autotools = AutoToolsBuildEnvironment(self)
+            args=["--without-caps"]
+            for lib in ['alsa', 'glib', 'fftw', 'x11', 'samplerate', 'openssl']:
+                args.append("--%s-%s" % ('enable' if getattr(self.options, 'with_' + lib) else 'disable', lib))
+            if self.options.shared:
+                args.extend(['--enable-shared=yes', '--enable-static=no'])
+            else:
+                args.extend(['--enable-shared=no', '--enable-static=yes'])
+            with tools.environment_append({"PKG_CONFIG_PATH": self.build_folder}):
+                with tools.environment_append({
+                        "FFTW_CFLAGS": tools.PkgConfig("fftw").cflags,
+                        "FFTW_LIBS": tools.PkgConfig("fftw").libs}):
+                    self._autotools.configure(args=args,  configure_dir=self._source_subfolder)
+        return self._autotools
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        
+        for package in self.deps_cpp_info.deps:
+            lib_path = self.deps_cpp_info[package].rootpath
+            for dirpath, _, filenames in os.walk(lib_path):
+                for filename in filenames:
+                    if filename.endswith('.pc'):
+                        shutil.copyfile(os.path.join(dirpath, filename), filename)
+                        tools.replace_prefix_in_pc_file(filename, lib_path)
+
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self._source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        autotools = self._configure_autotools()
+        autotools.install()
+        tools.rmdir(os.path.join(self.package_folder, 'etc'))
+        tools.rmdir(os.path.join(self.package_folder, 'share'))
+        tools.rmdir(os.path.join(self.package_folder, 'lib', 'cmake'))
+        tools.rmdir(os.path.join(self.package_folder, 'lib', 'pkgconfig'))
+        for f in glob.glob(os.path.join(self.package_folder, "lib", "**", "*.la"), recursive=True):
+            os.remove(f)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libdirs = ['lib', os.path.join('lib', 'pulseaudio')]
+        if self.options.with_glib:
+            self.cpp_info.libs.append('pulse-mainloop-glib')
+        self.cpp_info.libs.extend(['pulse-simple', 'pulse'])
+        if not self.options.shared:
+            self.cpp_info.libs.append('pulsecommon-%s' % self.version)
+        self.cpp_info.defines = ['_REENTRANT']
+        self.cpp_info.names['pkg_config'] = 'libpulse'
