4170d17944542cd32b73d6376003e22d9a0efa95
bincrafters-user <bincrafters@gmail.com>
2020-09-22 19:06:50 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.26.0

--- conanfile.py
+++ conanfile.py
@@ -55,7 +55,7 @@ class LibnameConan(ConanFile):
         if self.options.with_alsa:
             self.requires("libalsa/1.1.9")
         if self.options.with_glib:
-            self.requires("glib/2.65.1")
+            self.requires("glib/2.66.0")
         if self.options.with_fftw:
             self.requires("fftw/3.3.8")
         if self.options.with_x11:
